f88fd4640fbf1fdbead38f826bbdb5f6f41e4565
Christian Grothoff <christian@grothoff.org>
2020-05-03 20:12:18 +0200

    modify TALER_EXCHANGE_deposits_get() API to avoid NULL pointers and need for deep copies

--- src/testing/testing_api_cmd_deposits_get.c
+++ src/testing/testing_api_cmd_deposits_get.c
@@ -111,8 +111,7 @@ deposit_wtid_cb (void *cls,
   switch (hr->http_status)
   {
   case MHD_HTTP_OK:
-    GNUNET_assert (NULL != dd->wtid);
-    tts->wtid = *dd->wtid;
+    tts->wtid = dd->wtid;
     if (NULL != tts->bank_transfer_reference)
     {
       const struct TALER_TESTING_Command *bank_transfer_cmd;
@@ -140,11 +139,7 @@ deposit_wtid_cb (void *cls,
       }
 
       /* Compare that expected and gotten subjects match.  */
-<<<<<<< HEAD
       if (0 != GNUNET_memcmp (&dd->wtid,
-=======
-      if (0 != GNUNET_memcmp (dd->wtid,
->>>>>>> modify TALER_EXCHANGE_deposits_get() API to combine returned arguments into a struct (simplifies merchant later)
                               wtid_want))
       {
         GNUNET_break (0);
