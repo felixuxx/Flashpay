7332403bf0d72c9b1bb17348ef96b91615bcf525
Christian Grothoff <christian@grothoff.org>
2021-11-17 11:19:57 +0100

    -bump version

--- src/exchange/taler-exchange-httpd_keys.c
+++ src/exchange/taler-exchange-httpd_keys.c
@@ -50,7 +50,7 @@
  * #TALER_PROTOCOL_CURRENT and #TALER_PROTOCOL_AGE in
  * exchange_api_handle.c!
  */
-#define EXCHANGE_PROTOCOL_VERSION "9:0:0"
+#define EXCHANGE_PROTOCOL_VERSION "10:0:0"
 
 
 /**
