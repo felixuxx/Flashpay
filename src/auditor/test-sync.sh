80fc8231ade4e8581dc0447f31bd280bb9c41d3d
Thien-Thi Nguyen <ttn@gnuvola.org>
2022-03-02 17:32:52 -0500

    Don't hardcode directory in taler-auditor-sync(1) invocation

--- src/auditor/test-sync.sh
+++ src/auditor/test-sync.sh
@@ -16,7 +16,7 @@ echo -n "."
 psql talercheck-in < auditor-basedb.sql >/dev/null 2> /dev/null
 
 echo -n "."
-~/bin/taler-auditor-sync -s test-sync-in.conf -d test-sync-out.conf -t
+taler-auditor-sync -s test-sync-in.conf -d test-sync-out.conf -t
 
 # cs_nonce_locks excluded: no point
 for table in denominations denomination_revocations wire_targets reserves reserves_in reserves_close reserves_out auditors auditor_denom_sigs exchange_sign_keys signkey_revocations extensions extension_details known_coins refresh_commitments refresh_revealed_coins refresh_transfer_keys deposits refunds wire_out aggregation_tracking wire_fee recoup recoup_refresh
