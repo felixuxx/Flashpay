30b0302c6d23598374fd5ab9363eb2d718d7fc1e
Devan Carpenter <devan@taler.net>
2023-09-07 11:05:26 -0400

    ci: setup postgresql inside test job

--- ci/jobs/2-test/test.sh
+++ ci/jobs/2-test/test.sh
@@ -8,11 +8,15 @@ set -evu
 make
 make install
 
+sudo -u postgres /usr/lib/postgresql/15/bin/postgres -D /etc/postgresql/15/main -h localhost -p 5432 &
+sleep 10
+sudo -u postgres createuser -p 5432 root
+sudo -u postgres createdb -p 5432 -O root talercheck
 
 check_command()
 {
 	# Set LD_LIBRARY_PATH so tests can find the installed libs
-	LD_LIBRARY_PATH=/usr/local/lib make check
+	LD_LIBRARY_PATH=/usr/local/lib PGPORT=5432 make check
 }
 
 print_logs()
