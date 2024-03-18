f2d4ef798e68f1bf6ae0e158c6ccc13feeadbc33
Christian Grothoff <christian@grothoff.org>
2024-03-18 23:31:02 +0100

    make mustach test suite more robust

--- src/templating/run-original-tests.sh
+++ src/templating/run-original-tests.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+# This file is in the public domain.
 set -eu
+
+export CFLAGS="-g"
+
+function build()
+{
+    make clean
+    make
+}
+
+# Install rebuild-on-exit handler (except for kill -9)
+trap build EXIT
+
+make clean
+
 # The build fails if libjson-c-dev is not installed.
 # That's OK, we don't otherwise need it and don't
 # even bother testing for it in configure.ac.
 # However, in that case, skip the test suite.
-
-export CFLAGS="-g"
-
-make clean
 make -f mustach-original-Makefile mustach || exit 77
 make -f mustach-original-Makefile clean || true
 make -f mustach-original-Makefile basic-tests
 make -f mustach-original-Makefile clean || true
-make clean
-make
