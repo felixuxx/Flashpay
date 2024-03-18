3be1a278218dd7fa6239cafd7099f754fe256936
Christian Grothoff <christian@grothoff.org>
2024-03-18 23:34:57 +0100

    truly skip if libjson-c-dev is not present

--- src/templating/run-original-tests.sh
+++ src/templating/run-original-tests.sh
@@ -19,7 +19,7 @@ make clean
 # That's OK, we don't otherwise need it and don't
 # even bother testing for it in configure.ac.
 # However, in that case, skip the test suite.
-make -f mustach-original-Makefile mustach || exit 77
+make -f mustach-original-Makefile mustach mustach-json-c.o || exit 77
 make -f mustach-original-Makefile clean || true
 make -f mustach-original-Makefile basic-tests
 make -f mustach-original-Makefile clean || true
