e512fcafd95d9268541d3645ab56917f46f0c16f
Nullptrderef <nullptrderef@proton.me>
2024-06-16 16:11:15 +0200

    rm tmp file

--- confdefs.h
+++
@@ -1,7 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "taler-exchange"
-#define PACKAGE_TARNAME "taler-exchange"
-#define PACKAGE_VERSION "0.11.2"
-#define PACKAGE_STRING "taler-exchange 0.11.2"
-#define PACKAGE_BUGREPORT "taler-bug@gnunet.org"
-#define PACKAGE_URL ""
